49c1e2f03c512898dcf29f87edc0f74638f09d43
parthsujalshah <43181887+parthsujalshah@users.noreply.github.com>
2018-10-03 22:48:36 +0530

    Add files via upload

---
+++ test error contstant expr.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main()
+{
+	int n, i, j, c;
+	scanf("%d", &n);
+	int arr[n], sort[n];
+	
+	for(i = 0; i<n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	
+	for(j = 0; j < n; j++)
+	{
+	
+		int min = arr[0], max = arr[0];
+	
+		for(i = 0; i < n; i++)
+		{
+			if(arr[i] > max)
+			{
+				max = arr[i];	// MAX value
+			}
+		}
+		
+	
+		for(i = 0; i<n; i++)
+		{
+			if(arr[i] < min)
+			{
+				min = arr[i];	// MIN value
+				c = i;
+			}
+		}
+		
+		
+		sort[j] =  min;
+		arr[c] = max;
+			
+	}
+	
+	for(i = 0; i<n; i++)
+	{
+		printf("%d\t", sort[i]);
+	}     
+}
